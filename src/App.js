7332a83c4e72548505691886c2093b33c2142e90
DavidLiu <dvdgege5@gmail.com>
2018-03-11 14:58:29 -0700

    change default splash page; use classic styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from 'material-ui/Button';
+//import logo from './logo.svg';
+import './css/App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Pitlane</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href ="#APP-BODY">
+          <Button color="primary" variant="raised">Start</Button>
+        </a>
       </div>
     );
   }
