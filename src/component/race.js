ddfd9b40d4ef643f3553db17cb3428ca501724ce
DL <dvdgege5@gmail.com>
2018-03-16 12:12:15 -0700

    begin implementing race component/page

---
+++ src/component/race.js
@@ -0,0 +1,7 @@
+// stats by race homepage
+
+// React core
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link, Route, Switch, Redirect } from 'react-router-dom';
+import Plot from 'react-plotly.js';
